6f9c3015554228c11e0a64f132e0684491f4f406
smokhtari <sofiane.mokhtari@carians.fr>
2022-01-18 14:40:52 +0100

    register function

--- index.js
+++ index.js
@@ -7,38 +7,39 @@ const isEmail = (val) => {
     }
   }
 
-  module.exports = async function login(email, password) {
-    if(!isEmail(email) )
-    await axios.post("http://172.16.28.58:5001/api" + "/users/login", { email, password })
-    .then(res => {
-        console.log('ici', res)
-        if(res.data.data.verification)
-            return {verification : true}
-        else
-            return {verification: false}
-      })
-      .catch(err => {
-          console.log('ici', err)
-          console.log(err)
-      });
-  };
-
-  module.exports = async function register(email, firstname, lastname, password, pass2) {
-    if(pass !== pass2)
-        return setError("Les mots de passe ne correspondent pas")
-    if(lastname.length < 2 || firstname < 2) 
-        return setError("Le prénom ou le nom est trop court")
-  if(!isEmail(email))
-    await axios.post(
-        "http://172.16.28.58:5001/api" + "/users/create",
-        {
-            email,
-            firstname,
-            lastname,
-            password
-          }
-      ).then(res => {return true})
-      .catch(err => {
-        return err
-      });
-  };
+  module.exports = {
+      login:   async function(email, password) {
+        if(!isEmail(email) )
+        await axios.post("http://172.16.28.58:5001/api" + "/users/login", { email, password })
+        .then(res => {
+            console.log('ici', res)
+            if(res.data.data.verification)
+                return {verification : true}
+            else
+                return {verification: false}
+          })
+          .catch(err => {
+              console.log('ici', err)
+              console.log(err)
+          });
+      },
+      register: async function(email, firstname, lastname, password, pass2) {
+        if(pass !== pass2)
+            return setError("Les mots de passe ne correspondent pas")
+        if(lastname.length < 2 || firstname < 2) 
+            return setError("Le prénom ou le nom est trop court")
+      if(!isEmail(email))
+        await axios.post(
+            "http://172.16.28.58:5001/api" + "/users/create",
+            {
+                email,
+                firstname,
+                lastname,
+                password
+              }
+          ).then(res => {return true})
+          .catch(err => {
+            return err
+          });
+      }
+  }
