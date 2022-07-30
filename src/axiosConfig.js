01b1b059b8277c5ea6a5d9d72ec1383376cdfdd9
Mokhtari Sofiane <mokhta_s@etna-alternance.net>
2022-07-30 18:25:54 +0200

    use es6 / modif structure

---
+++ src/axiosConfig.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+    
+export const httpClient = axios.create({
+    baseURL: "https://api.preprod.statfive.fr/api",
+    // baseURL: process.env.APP_API_BASE_URL,
+});
+
+// httpClient.interceptors.request.use(function (config) {
+//     const token = localStorage.getItem('token');
+//     config.headers['api-token'] =  token ? `Bearer ${token}` : '';
+// });
