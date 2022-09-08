8dfaf09543d1ae1b9f89bd0d830212b19be55d72
Mokhtari Sofiane <mokhta_s@etna-alternance.net>
2022-09-08 19:31:29 +0200

    axios config url

--- dist/axiosConfig.js
+++ dist/axiosConfig.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.httpClient = undefined;
 
-var _axios = require("axios");
+var _axios = require('axios');
 
 var _axios2 = _interopRequireDefault(_axios);
 
-var _asyncStorage = require("@react-native-async-storage/async-storage");
+var _asyncStorage = require('@react-native-async-storage/async-storage');
 
 var _asyncStorage2 = _interopRequireDefault(_asyncStorage);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var httpClient = exports.httpClient = _axios2.default.create({
-    baseURL: "https://api.preprod.statfive.fr/api"
-    // baseURL: process.env.APP_API_BASE_URL,
+    //baseURL: "https://api.preprod.statfive.fr/api",
+    baseURL: process.env.API_URL
 });
 
 httpClient.interceptors.request.use(async function (config) {
--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
     
 export const httpClient = axios.create({
-    baseURL: "https://api.preprod.statfive.fr/api",
-    // baseURL: process.env.APP_API_BASE_URL,
+    //baseURL: "https://api.preprod.statfive.fr/api",
+    baseURL: process.env.API_URL,
 });
 
 httpClient.interceptors.request.use(async function (config) {
